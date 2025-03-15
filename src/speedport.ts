7d42dafe397ade99827eb4015858081025b3e015
David Adamson <david.adamson2304@gmail.com>
2025-03-15 21:27:04 +0100

    feat: reboot speedport

--- src/speedport.ts
+++ src/speedport.ts
@@ -1,7 +1,23 @@
+import puppeteer from "puppeteer";
+
 export class Speedport {
   constructor(private readonly password: string) {}
 
   async reboot() {
-    // TODO: use puppeteer to reboot speedport
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto("http://speedport.ip/html/login/index.html");
+    await page.waitForSelector("#router_password", { visible: true });
+    await page.type("#router_password", this.password);
+    await page.click("#loginbutton");
+    await page.waitForNavigation();
+
+    await page.goto("http://speedport.ip/html/content/config/problem_handling.html");
+    await page.waitForSelector("h2[data-i18n=key_startNew]", { visible: true });
+    await page.click("h2[data-i18n=key_startNew]");
+    await page.click("#btn_reboot");
+
+    await browser.close();
   }
 }
