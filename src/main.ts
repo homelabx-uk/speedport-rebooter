5c0d6b98f5ff8496196d7acec0bd491ecd22c4d1
David Adamson <david.adamson2304@gmail.com>
2025-03-16 19:51:09 +0100

    feaT: graceful shutdown and better logging

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,29 @@ import { Speedport } from "./speedport";
 import { isInternetConnected } from "@/is-internet-connected";
 
 const speedport = new Speedport(process.env.SPEEDPORT_PASSWORD!);
-const reboots = new Array<{ date: Date; rebootSuccess: boolean }>();
 
-await speedport.reboot();
-await new Promise((res) => setTimeout(res, 10 * 60 * 1000));
-
-// TODO add reporting
-/*const _reboots = reboots.splice(0);
-console.log(`network connection was lost ${_reboots.length} times.`);
-console.log(`reboots were `);*/
+let offline = false;
 
 async function check() {
   if (await isInternetConnected()) {
+    if (offline) {
+      console.log("we are back online again."); // TODO send to discord
+      offline = false;
+    }
     setTimeout(check, 60 * 1000); // 1 minute
   } else {
+    offline = true;
     console.error("internet connection lost. rebooting speedport...");
     try {
       await speedport.reboot();
-      reboots.push({ date: new Date(), rebootSuccess: true });
     } catch (e) {
-      reboots.push({ date: new Date(), rebootSuccess: false });
       console.error("failed to reboot speedport", e);
     }
     setTimeout(check, 10 * 60 * 1000); // 10 minutes
   }
 }
 check();
+
+process.on("SIGTERM", () => {
+  process.exit();
+});
